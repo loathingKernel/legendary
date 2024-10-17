65a0874a3ae19e4d724e553017344529880b121b
Mathis Dröge <mathis@commandmc.de>
2024-10-17 23:31:01 +0200

    Add conflict resolution based on last sync time (#10)
    * feat: add conflict resolution based on last sync time
    
    * improv: swap the order of conflict responses
    
    users may associate this with the order in which we display timestamps
    
    ---------
    
    Co-authored-by: Paweł Lidwin <lidwinpawel@gmail.com>

--- legendary/models/game.py
+++ legendary/models/game.py
@@ -193,6 +193,7 @@ class InstalledGame:
     requires_ot: bool = False
     save_path: Optional[str] = None
     is_preloaded: bool = False
+    save_timestamp: Optional[float] = None
 
     @classmethod
     def from_json(cls, json):
@@ -213,6 +214,7 @@ class InstalledGame:
         tmp.requires_ot = json.get('requires_ot', False)
         tmp.is_dlc = json.get('is_dlc', False)
         tmp.save_path = json.get('save_path', None)
+        tmp.save_timestamp = json.get('save_timestamp', None)
         tmp.manifest_path = json.get('manifest_path', '')
         tmp.needs_verification = json.get('needs_verification', False) is True
         tmp.platform = json.get('platform', 'Windows')
@@ -239,6 +241,7 @@ class SaveGameStatus(Enum):
     REMOTE_NEWER = 1
     SAME_AGE = 2
     NO_SAVE = 3
+    CONFLICT = 4
 
 
 class VerifyResult(Enum):
